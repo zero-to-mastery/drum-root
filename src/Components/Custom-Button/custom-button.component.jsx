5458ba6af9012cd33ef0d9e32aa6de323d20eb96
Samir <42714951+sameerrM@users.noreply.github.com>
2019-11-10 00:28:02 +0100

    renamed components

--- src/Components/Custom-Button/custom-button.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CustomButton = ({ title, className }) => {
-    return (
-        <button className={className}>{title}</button>
-    )
-}
-
-export default CustomButton
--- src/Components/Loader/index.js
+++ src/Components/Loader/index.js
@@ -1,18 +1,17 @@
-import React from 'react'
-import styled from 'styled-components'
-import { keyframes } from '@emotion/core'
+import React from "react";
+import styled, { keyframes } from "styled-components";
 
 const rotateWrapp = keyframes`
   100% {
     transform: rotate(360deg);
   }
-`
+`;
 
 const rotateDot = keyframes`
  80%, 100% {
     transform: rotate(360deg); 
   } 
-`
+`;
 
 const dotBefore = keyframes`
   50% {
@@ -20,37 +19,35 @@ const dotBefore = keyframes`
   } 100%, 0% {
     transform: scale(1.0); 
   } 
-`
+`;
 
 const Wrapper = styled.div`
   width: 1.25rem;
   height: 1.25rem;
   position: relative;
-  animation:${rotateWrapp} 2.5s infinite linear both;
-`
+  animation: ${rotateWrapp} 2.5s infinite linear both;
+`;
 
 const SkChaseDot = styled.div`
   width: 100%;
   height: 100%;
   position: absolute;
   left: 0;
-  top: 0; 
-  animation: ${rotateDot} 2.0s infinite ease-in-out both; 
+  top: 0;
+  animation: ${rotateDot} 2s infinite ease-in-out both;
   animation-delay: ${props => props.animationDelay};
 
-&:before {
-  content: '';
-  display: block;
-  width: 25%;
-  height: 25%;
-  background-color: red;
-  border-radius: 100%;
-  animation: ${dotBefore} 2.0s infinite ease-in-out both;
-  animation-delay: ${props => props.animationDelay};
-}
-`
-
-
+  &:before {
+    content: "";
+    display: block;
+    width: 25%;
+    height: 25%;
+    background-color: red;
+    border-radius: 100%;
+    animation: ${dotBefore} 2s infinite ease-in-out both;
+    animation-delay: ${props => props.animationDelay};
+  }
+`;
 
 const Loader = () => {
   return (
@@ -62,7 +59,7 @@ const Loader = () => {
       <SkChaseDot animationDelay="-0.7s" />
       <SkChaseDot animationDelay="-0.6s" />
     </Wrapper>
-  )
-}
+  );
+};
 
-export default Loader
+export default Loader;
