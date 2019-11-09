9748d0ee84365cba023c939507e0ffca59fc73f6
Samir <42714951+sameerrM@users.noreply.github.com>
2019-11-09 11:24:35 +0100

    custom button

---
+++ src/Components/Custom-Button/custom-button.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomButton = ({ title, className }) => {
+    return (
+        <button className={className}>{title}</button>
+    )
+}
+
+export default CustomButton
