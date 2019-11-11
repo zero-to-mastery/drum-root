8c034f9072aff24776750aee0259811c102b88c4
Giuliano Cernada <giuliano.cernada@hotmail.com>
2019-11-11 11:49:02 -0300

    added styling to button component

--- src/Components/Custom-Button/index.js
+++ src/Components/Custom-Button/index.js
@@ -1,4 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+const button = styled.button`
+  color: #aa3939;
+  font-size: 1em;
+  margin: 0.5em;
+  padding: 0.25em 1em;
+  border: 3px solid #aa3939;
+  border-radius: 5px;
+`;
+
+CustomButton.propTypes = {
+  title: PropTypes.string,
+  className: PropTypes.string
+};
 
 const CustomButton = ({ title, className }) => {
   return <button className={className}>{title}</button>;
