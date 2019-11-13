ec798e4e6dd582fa68557a83f99ba513f71de4d4
Giuliano Cernada <giuliano.cernada@hotmail.com>
2019-11-13 10:38:44 -0300

    back

--- src/Components/Custom-Button/index.js
+++ src/Components/Custom-Button/index.js
@@ -1,7 +1,28 @@
 import React from "react";
+import styled, { css } from "styled-components";
+import PropTypes from "prop-types";
 
-const CustomButton = ({ title, className }) => {
-  return <button className={className}>{title}</button>;
+const Button = styled.button`
+  color: ${props => (props.color ? props.color : "#000")};
+  font-size: 1rem;
+  margin: 0.5rem;
+  padding: 0.25rem 1rem;
+  border: 0.2rem solid ${props => (props.color ? props.color : "#000")};
+  border-radius: 0.2rem;
+`;
+
+const CustomButton = ({ title, className, color }) => {
+  return (
+    <Button color={color} className={className}>
+      {title}
+    </Button>
+  );
+};
+
+CustomButton.propTypes = {
+  title: PropTypes.string,
+  className: PropTypes.string,
+  color: PropTypes.string
 };
 
 export default CustomButton;
