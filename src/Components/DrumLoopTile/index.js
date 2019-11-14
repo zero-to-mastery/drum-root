00ca29d14c71c8b034ec3b71052c62e929ec663d
talk2coded <emmanuelsteve43@gmail.com>
2019-11-14 03:08:30 +0100

    latest commit

--- src/Components/DrumLoopTile/index.js
+++ src/Components/DrumLoopTile/index.js
@@ -5,13 +5,13 @@ const TitleContainer = styled.div`
   border: 0.063rem solid black;
   width: 12.5rem;
   text-align: center;
-  height: 100px;
-  padding: 10px;
+  height: 6.25rem;
+  padding: 0.625rem;
 `;
 
 const TitleLink = styled.a`
-  border: 1px solid black;
-  padding: 5px;
+  border: 0.063rem solid black;
+  padding: 0.313rem;
 `;
 
 const LoopTile = ({ title, href }) => (
