511cf75269c965c70892cd895dc95a4b34959e71
Marco Seoane <marcoseoane@Marcos-MacBook-Pro.local>
2019-11-15 10:33:24 +0000

    changes requested by gavin

--- src/Components/SoundUploader/index.js
+++ src/Components/SoundUploader/index.js
@@ -7,13 +7,17 @@ const SoundUploader = ({}) => {
   const inputEl = useRef(null);
 
   const uploadSound = event => {
-    const file = event.target.files[0];
-    if (file.size > 1000000) {
-      // possibly change this to a flash-message for better UX/UI in the future
-      alert("File size must be less than 1MB");
-      inputEl.current.value = "";
-    } else {
-      // add sound to a container component that stores users custom sounds??
+    try {
+      const file = event.target.files[0];
+      if (file.size > 1000000) {
+        // possibly change this to a flash-message for better UX/UI in the future
+        alert("File size must be less than 1MB");
+        inputEl.current.value = "";
+      } else {
+        // add sound to a container component that stores users custom sounds??
+      }
+    } catch (err) {
+      alert("Something went wrong with file upload");
     }
   };
 
