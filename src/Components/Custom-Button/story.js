71831b92fe092b0d264a15b9254fd39a6194d281
Giuliano Cernada <giuliano.cernada@hotmail.com>
2019-11-15 20:56:03 -0300

    changes

--- src/Components/Custom-Button/story.js
+++ src/Components/Custom-Button/story.js
@@ -1,23 +1,13 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { withKnobs, color, text } from "@storybook/addon-knobs";
-// import { action } from "@storybook/addon-actions";
 import CustomButton from "./index";
-
-// export const defaultButton = {
-//   color: "#C13F3F",
-//   className: "",
-//   title: "Custom Button",
-//   bgColor: "transparent"
-// };
 
 const returnKnobs = () => ({
   color: color("Color"),
   className: text("Class"),
   title: text("Button text"),
   bgColor: color("Background Color")
-  // const defaultValue = '#ff00ff';
-  // const value = color(defaultValue);
 });
 
 storiesOf("CustomButton", module)
