bb4cffa5c987da53763678e72be3baf1370341d9
talk2coded <emmanuelsteve43@gmail.com>
2019-11-25 02:08:10 +0100

    comments removed

--- src/Components/DrumLoopTile/index.test.js
+++ src/Components/DrumLoopTile/index.test.js
@@ -8,14 +8,6 @@ import Adapter from "enzyme-adapter-react-16";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-/*it("renders correctly enzyme", () => {
-    const wrapper = shallow(<LoopTile />)
-  
-    expect(toJson(wrapper)).toMatchSnapshot();
-  });*/
-// incorrect function assignment in the onClick method
-// will still pass the tests.
-
 test("renders correctly enzyme", () => {
   const wrapper = mount(<LoopTile />);
 
