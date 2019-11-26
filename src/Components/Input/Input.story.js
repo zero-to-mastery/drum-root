f76f0e6a73d0c801f1e42669f73ab901dff7d6d7
Bocong Zhao <zhaobocong@hotmail.com>
2019-11-26 14:21:45 -0500

    Added storybook for Input Component

---
+++ src/Components/Input/Input.story.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Input from "./index";
+
+storiesOf("Input", module).add("with default", () => {
+  return <Input />;
+});
