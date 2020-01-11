f1d534f3f05f0f912f798d4069a45dde6e622073
adrianmonteil1983 <monteiladrian@gmail.com>
2020-01-11 15:30:49 -0500

    add constants file

---
+++ src/constants/constants.js
@@ -0,0 +1,13 @@
+/************************************* COLORS ***********************************************/
+export const black = "#333333";
+export const white = "#FFFFFF";
+
+export const primary900 = "#361C4E";
+export const primary800 = "#4F2A73";
+export const primary700 = "#693797";
+export const primary600 = "#8244BC";
+export const primary500 = "#9B51E0";
+export const primary400 = "#B37AE7";
+export const primary300 = "#CBA4EF";
+export const primary200 = "#E2CDF6";
+export const primary100 = "#FAF6FE";
